7d50736d9c81a1eb197fefdf3bf97c3ea10501ba
Mustafa Halit Özer <mustafahalitozer@gmail.com>
2021-04-04 15:18:46 +0300

    04.04.2021 15:18

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Product } from 'src/app/models/product';
 import { ProductResponseModel } from 'src/app/models/productResponseModel';
 import {HttpClient} from '@angular/common/http';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-product',
@@ -11,20 +12,19 @@ import {HttpClient} from '@angular/common/http';
 export class ProductComponent implements OnInit {
 
   products:Product[] = [ ]
-  apiUrl ='https://localhost:44301/api/Products/getall';
+  dataLoaded = false;
 
-  constructor(private httpClient:HttpClient) { }
+  constructor(private productService:ProductService) { }
 
   ngOnInit(): void {
     this.getProducts();
   }
 
   getProducts(){
-    this.httpClient
-    .get<ProductResponseModel>(this.apiUrl)
-    .subscribe((response) => {
-      this.products = response.data;
-    });
+ this.productService.getProducts().subscribe(response => {
+  this.products = response.data;
+  this.dataLoaded = true;
+ })
   }
 
 }
