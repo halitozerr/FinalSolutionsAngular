254b0a8d0fe5c1463e744b0a9b3a5d569543e6f3
Mustafa Halit Özer <mustafahalitozer@gmail.com>
2021-04-05 16:58:02 +0300

    05.04.2021 16:57

--- src/app/components/category/category.component.ts
+++ src/app/components/category/category.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Category } from 'src/app/models/category';
+import { CategoryService } from 'src/app/services/category.service';
 
 @Component({
   selector: 'app-category',
@@ -7,20 +9,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoryComponent implements OnInit {
 
-  category1 = {categoryId : 3, categoryName:"Kozmetik"}
-  category2 = {categoryId : 3, categoryName:"Takı Tasarım"}
-  category3 = {categoryId : 3, categoryName:"Elektronik"}
-  category4 = {categoryId : 3, categoryName:"Araç"}
-  category5 = {categoryId : 3, categoryName:"Erkek Giyim"}
-  category6 = {categoryId : 3, categoryName:"Bayan Giyim"}
-  category7 = {categoryId : 3, categoryName:"Mutfak"}
-  category8 = {categoryId : 3, categoryName:"Spor"}
-  category9 = {categoryId : 3, categoryName:"Okul"}
+categories:Category[] = [];
 
-  categories = [this.category1,this.category2,this.category3,this.category4,this.category5,this.category6,this.category7,this.category8,this.category9]
-  constructor() { }
+  constructor(private categoryService:CategoryService) { }
 
   ngOnInit(): void {
+  this.getCategories();
+  }
+
+  getCategories(){
+    this.categoryService.getCategories().subscribe(response => {
+      this.categories = response.data
+    })
   }
 
 }
